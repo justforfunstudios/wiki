fff6a1b9507289962f8ea1ff7beaf3f2a92fbb6d
Cosmos <andreipavelescu2012@yahoo.com>
2025-01-20 20:52:57 +0200

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -10,7 +10,7 @@ import {themes as prismThemes} from 'prism-react-renderer';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'JustForFun Wiki',
+  title: ' Wiki',
   tagline: 'The official JustForFun Wiki Page 🔥',
   favicon: 'img/faviconjff.png',
 
