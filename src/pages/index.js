b25a37cc1588f5b919167c3effdbde9061331f76
Cosmos <andreipavelescu2012@yahoo.com>
2025-03-02 17:29:39 +0200

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -20,7 +20,7 @@ function HomepageHeader() {
           <Link
             className="button button--secondary button--lg"
             to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
+            Explore 🚀
           </Link>
         </div>
       </div>
@@ -32,8 +32,8 @@ export default function Home() {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      title={`JustForFun Wiki}`}
+      description="The official JustForFun Wiki Page 🔥">
       <HomepageHeader />
       <main>
         <HomepageFeatures />
